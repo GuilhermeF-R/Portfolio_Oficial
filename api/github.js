d30c132107921fcca52bf7f2ae8180f76d9551f2
GuilhermeF-R <Frodriguesguilherme18@gmail.com>
2025-01-30 15:47:55 -0300

    teste

--- api/github.js
+++ api/github.js
@@ -1,6 +1,6 @@
 // Função para buscar a descrição (About) do repositório no GitHub
 async function fetchGitHubAbout(repoName) {
-    const apiUrl = `https://api.github.com/repos/GuilhermeF-R/${repoName}`;
+    const apiUrl = `https://github.com/GuilhermeF-R/${repoName}`;
     const timeout = 10000; // 10 segundos
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), timeout);
