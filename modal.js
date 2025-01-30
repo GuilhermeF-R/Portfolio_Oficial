cee54c6b6416c3fd1abb494811fc9ed37e31e62d
GuilhermeF-R <Frodriguesguilherme18@gmail.com>
2025-01-29 22:56:04 -0300

    restart github about

--- modal.js
+++ modal.js
@@ -64,19 +64,17 @@ document.addEventListener('DOMContentLoaded', () => {
     // Função para buscar a descrição (About) do repositório no GitHub
     async function fetchGitHubAbout(repoName) {
         const apiUrl = `https://api.github.com/repos/GuilhermeF-R/${repoName}`;
-        const githubToken = process.env.MYTOKEN; // Substitua pelo seu token pessoal
 
         try {
             const response = await fetch(apiUrl, {
-                headers: {
-                    "Accept": "application/vnd.github.v3+json",
-                    "Authorization": `token ${githubToken}`
-                }
+                headers: { Accept: "application/vnd.github.v3+json" }
             });
 
             if (!response.ok) throw new Error("Não foi possível carregar a descrição.");
 
             const data = await response.json();
+
+            // Retorna a descrição (About) do repositório
             return data.description || "Nenhuma descrição disponível.";
         } catch (error) {
             console.error("Erro ao buscar descrição do GitHub:", error);
